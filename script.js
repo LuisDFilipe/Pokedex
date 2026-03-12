627ac44654df5303ced56d95b5b16cf0185ce290
Luis Pereira <luis.d.filipe.97@gmail.com>
2026-03-12 15:03:56 +0000

    added pokemon modal

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", init);
 
 const list_container = document.getElementById("pokedex-list");
+const filter_modal = document.getElementById("filter-modal");
+const pokemon_modal = document.getElementById("pokemon-modal");
 
 let pokedex_data = null;
 let hide_gender_diffs = true;
 let select_filters = [];
+let pokemon = {}
 
 const type_colors = {
   Normal: "#A8A77ABE",
@@ -31,22 +34,22 @@ function init() {
   fetch("pokedex.json")
     .then((response) => response.json())
     .then((data) => {
-      pokedex_data = data.pokedex.pokemon;
+      pokedex_data = data.pokedex.pokemon.flatMap((p) => p.forms);
       fetch_pokemon();
     })
     .catch((error) => console.error("Error loading JSON:", error));
 
-  // Open and close modal
-  const modal = document.getElementById("filter-modal");
+  // Open and close modals
   document.getElementById("filter-btn").addEventListener("click", () => {
-    modal.style.display = "flex";
+    filter_modal.style.display = "flex";
   });
   document.getElementById("filter-modal-close-btn").addEventListener("click", () => {
-    modal.style.display = "none";
+    filter_modal.style.display = "none";
   });
 
   window.addEventListener("click", (e) => {
-    if (e.target == modal) modal.style.display = "none";
+    if (e.target == filter_modal) filter_modal.style.display = "none";
+    if (e.target == pokemon_modal) pokemon_modal.style.display = "none";
   });
 
   // Apply filters
@@ -83,7 +86,6 @@ function fetch_pokemon() {
   let show_shiny_sprites = select_filters.includes("show_shiny_sprites");
 
   pokedex_data
-    .flatMap((p) => p.forms)
     .filter((p) => !hide_all_forms || (hide_all_forms && p.form == null))
     .filter((p) => !hide_genders || (hide_genders && p.gender != "female"))
     .filter((p) => !hide_forms_without_gender_differences || (hide_forms_without_gender_differences && p.has_gender_difference != false))
@@ -92,19 +94,67 @@ function fetch_pokemon() {
     .filter((p) => !hide_gigantamax_forms || (hide_gigantamax_forms && p.form != "gigantamax"))
     .forEach((pokemon) => {
       const div = document.createElement("div");
+      div.id = pokemon.id;
       div.className = "pokemon-div";
-      div.style = `background: linear-gradient(to right, ${type_colors[pokemon.type1]} 50%, ${type_colors[pokemon.type2 ?? pokemon.type1]} 50%);`
+      div.style = `background: linear-gradient(to right, ${type_colors[pokemon.type1]} 50%, ${type_colors[pokemon.type2 ?? pokemon.type1]} 50%);`;
+      
+      div.addEventListener("click", () => {
+        pokemon_modal.style.display = "flex";
 
+        let pokemon_modal_content = pokemon_modal.querySelector("#pokemon-modal-content");
+        pokemon_modal_content.innerHTML = "";
+
+        // Pokemon Modal Close
+        const modal_close = document.createElement("span");
+        modal_close.id = "pokemon-modal-close-btn";
+        modal_close.className = "close";
+        modal_close.innerHTML = "&times;";
+        modal_close.addEventListener("click", () => {
+          pokemon_modal.style.display = "none";
+        });
+
+        // Pokemon Modal Image
+        const modal_image = document.createElement("img");
+        modal_image.className = "pokemon-img";
+
+        set_pokemon_image(modal_image, pokemon, show_shiny_sprites);
+
+        // Pokemon Modal Name
+        const modal_name = document.createElement("h2");
+        modal_name.className = "pokemon-text";
+        modal_name.textContent = pokemon.name;
+
+        // Pokemon Modal Types
+        const modal_types_div = document.createElement("div");
+        modal_types_div.className = "pokemon-types-div";
+        
+        const type1_tag = document.createElement("span");
+        type1_tag.className = "type-badge";
+        type1_tag.textContent = (pokemon.type1 ?? "").toUpperCase();
+        type1_tag.style = `background-color: ${type_colors[pokemon.type1]};`
+
+        const type2_tag = document.createElement("span");
+        type2_tag.className = "type-badge";
+        type2_tag.textContent = (pokemon.type2 ?? "").toUpperCase();
+        type2_tag.style = `background-color: ${type_colors[pokemon.type2]};`
+        
+        // Pokemon Modal Appends
+        if(pokemon.type1) modal_types_div.appendChild(type1_tag);
+        if(pokemon.type2) modal_types_div.appendChild(type2_tag);
+
+        pokemon_modal_content.appendChild(modal_close);
+        pokemon_modal_content.appendChild(modal_name);
+        pokemon_modal_content.appendChild(modal_types_div);
+        pokemon_modal_content.appendChild(modal_image);
+      });
+
+      // Pokemon Image
       const img = document.createElement("img");
       img.className = "pokemon-img";
 
-      let sprite = `sprites/pokemon/${show_shiny_sprites ? "shiny" : "normal"}/${pokemon.sprite}.png`;
+      set_pokemon_image(img, pokemon, show_shiny_sprites);
 
-      img.src = sprite;
-      img.onerror = () => {
-        img.src = "assets/0000.png";
-      };
-      
+      // Pokemon Gender
       const corner_img = document.createElement("img");
       corner_img.className = "corner-img";
 
@@ -113,14 +163,17 @@ function fetch_pokemon() {
       else if(pokemon.gender == "female")
         corner_img.src = "assets/female.png";
 
+      // Pokemon Number
       const corner_text = document.createElement("span");
       corner_text.className = "corner-text";
       corner_text.textContent = "#" + pokemon.id.substring(0, 4);
       
+      // Pokemon Name
       const name = document.createElement("span");
       name.className = "pokemon-text";
       name.textContent = pokemon.name;
 
+      // Pokemon Div Appends
       div.appendChild(corner_text);
       div.appendChild(img);
       if (pokemon.gender && !hide_genders && (!hide_forms_without_gender_differences || (hide_forms_without_gender_differences && pokemon.show_gender != false))) {
@@ -130,4 +183,13 @@ function fetch_pokemon() {
 
       list_container.appendChild(div);
     });
+}
+
+function set_pokemon_image(img, pokemon, show_shiny_sprites) {
+  let sprite = `sprites/pokemon/${show_shiny_sprites ? "shiny" : "normal"}/${pokemon.sprite}.png`;
+
+  img.src = sprite;
+  img.onerror = () => {
+    img.src = "assets/0000.png";
+  };
 }
